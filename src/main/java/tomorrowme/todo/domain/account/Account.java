85833e4a3566499c500b7b998d278efd222cbee6
cousim46 <wjdghldns24@naver.com>
2024-05-19 01:40:55 +0900

    feat : 회원 엔티티 생성 및 회원 생성 팩토리 메서드 생성

---
+++ src/main/java/tomorrowme/todo/domain/account/Account.java
@@ -0,0 +1,30 @@
+package tomorrowme.todo.domain.account;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tomorrowme.todo.domain.BaseEntity;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Account extends BaseEntity {
+
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String salt;
+
+    private Account(String phone, String password, String salt) {
+        this.phone = phone;
+        this.password = password;
+        this.salt = salt;
+    }
+    public static Account singUp(String phone, String encryPassword, String salt) {
+        return new Account(phone, encryPassword, salt);
+    }
+}
