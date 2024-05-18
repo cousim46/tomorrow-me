d4ed32a1f890b6f16bba295417c166b5576d1ba4
cousim46 <wjdghldns24@naver.com>
2024-05-19 00:17:36 +0900

    feat : 컬럼 속성 설정

--- src/main/java/tomorrowme/todo/domain/BaseEntity.java
+++ src/main/java/tomorrowme/todo/domain/BaseEntity.java
@@ -1,5 +1,6 @@
 package tomorrowme.todo.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,7 @@ public abstract class BaseEntity {
     private Long id;
 
     @CreatedDate
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
