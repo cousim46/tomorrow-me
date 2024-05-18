aa0c5cda7a68bacb48d7443940a78dbb9e47649a
cousim46 <wjdghldns24@naver.com>
2024-05-18 23:57:33 +0900

    feat : Auditing 요소 추가

--- src/main/java/tomorrowme/todo/domain/BaseEntity.java
+++ src/main/java/tomorrowme/todo/domain/BaseEntity.java
@@ -1,6 +1,9 @@
 package tomorrowme.todo.domain;
 
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -12,6 +15,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @CreatedDate
     private LocalDateTime createdAt;
 
