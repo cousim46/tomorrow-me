1b8b5b446cff76e85eb895e4253a7771df8fdb6d
cousim46 <wjdghldns24@naver.com>
2024-05-20 19:02:42 +0900

    fix : 회원에 대한 컬럼 변경 및 전화번호에 대한 유니크 제약조건 설정

--- src/main/java/tomorrowme/todo/domain/account/Account.java
+++ src/main/java/tomorrowme/todo/domain/account/Account.java
@@ -2,6 +2,8 @@ package tomorrowme.todo.domain.account;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,21 +12,26 @@ import tomorrowme.todo.domain.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@Table(
+    uniqueConstraints = @UniqueConstraint(
+        name = "phone",
+        columnNames = "phone"
+    )
+)
 public class Account extends BaseEntity {
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String phone;
     @Column(nullable = false)
-    private String password;
-    @Column(nullable = false)
-    private String salt;
+    private String keyword;
 
-    private Account(String phone, String password, String salt) {
+    private Account(String phone, String keyword) {
         this.phone = phone;
-        this.password = password;
-        this.salt = salt;
+        this.keyword = keyword;
+
     }
-    public static Account singUp(String phone, String encryptPassword, String salt) {
-        return new Account(phone, encryptPassword, salt);
+
+    public static Account singUp(String phone, String keyword) {
+        return new Account(phone,keyword);
     }
 }
