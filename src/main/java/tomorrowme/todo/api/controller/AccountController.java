b61acc50fee83a8f9b99fa3b429c2524c4bd366e
cousim46 <wjdghldns24@naver.com>
2024-05-20 19:46:20 +0900

    feat : 회원가입 API

---
+++ src/main/java/tomorrowme/todo/api/controller/AccountController.java
@@ -0,0 +1,23 @@
+package tomorrowme.todo.api.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tomorrowme.todo.api.controller.dto.request.SignUp;
+import tomorrowme.todo.api.service.AccountWriteService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+
+    private final AccountWriteService accountWriteService;
+
+    @PostMapping
+    public void signUp(@Valid @RequestBody SignUp signUp) {
+        accountWriteService.singUp(signUp.getPhone(),signUp.getKeyword());
+    }
+}
