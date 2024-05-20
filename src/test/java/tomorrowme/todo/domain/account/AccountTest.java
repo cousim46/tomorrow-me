b7dd1fb95767f2d7e7b4ee4d116f2c5e48345a9a
cousim46 <wjdghldns24@naver.com>
2024-05-20 20:19:06 +0900

    test : 기상 시간, 취침시간 추가로 인한 수정

--- src/test/java/tomorrowme/todo/domain/account/AccountTest.java
+++ src/test/java/tomorrowme/todo/domain/account/AccountTest.java
@@ -2,20 +2,23 @@ package tomorrowme.todo.domain.account;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
 
-    @DisplayName("핸드폰 번호와 키워드로 회원을 생성한다.")
+    @DisplayName("핸드폰 번호와 키워드, 기상시간, 취침시간 데이터로 회원을 생성한다.")
     @Test
     void signUp() {
         //given
         String phone = "01012341234";
         String keyword = "keyword";
+        LocalTime wakeUpTime = LocalTime.of(9,0,0);
+        LocalTime sleepTime = LocalTime.of(2,0,0);
 
         //when
-        Account account = Account.singUp(phone, keyword);
+        Account account = Account.singUp(phone, keyword, wakeUpTime, sleepTime);
 
         //then
         assertThat(account).isNotNull();
