cb2d19b5652a1879ccaa71a804f0a0e9f77d363d
cousim46 <wjdghldns24@naver.com>
2024-05-21 00:13:22 +0900

    feat : 에러코드 추가

--- src/main/java/tomorrowme/todo/exception/TomorrowErrorCode.java
+++ src/main/java/tomorrowme/todo/exception/TomorrowErrorCode.java
@@ -1,7 +1,5 @@
 package tomorrowme.todo.exception;
 
-import static org.springframework.http.HttpStatus.*;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum TomorrowErrorCode {
-    ;
+    NOT_EXIST_INFO(HttpStatus.BAD_REQUEST, "존재 하지 않는 정보입니다.");
     private final HttpStatus status;
     private final String message;
 }
