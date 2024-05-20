f96e33acaf5b109f92f0a3e31599ea48a5b6d339
cousim46 <wjdghldns24@naver.com>
2024-05-20 19:02:02 +0900

    fix : 공통 응답형식에 따른 불필요한 클래스 삭제 및 수정

--- src/main/java/tomorrowme/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/tomorrowme/todo/exception/GlobalExceptionHandler.java
@@ -1,16 +1,18 @@
 package tomorrowme.todo.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tomorrowme.todo.api.controller.dto.response.CommonResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TomorrowException.class)
-    public ResponseEntity<TomorrowExceptionResponse> responseTomorrowException(TomorrowException exception) {
-        TomorrowExceptionResponse response = TomorrowExceptionResponse.response(exception);
-        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
+    public ResponseEntity<CommonResponse<Object>> responseTomorrowException(
+        TomorrowException exception) {
+        return new ResponseEntity<>(
+            CommonResponse.of(exception.getExceptionStatus(), exception.getExceptionMessage()),
+            exception.getExceptionStatus());
     }
 }
