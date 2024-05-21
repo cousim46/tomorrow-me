cba6fa374b7de84f07f4cbbda5023e16796fcc88
cousim46 <wjdghldns24@naver.com>
2024-05-22 00:06:21 +0900

    test : 보관함 조회에 대한 컨트롤러 테스트

--- src/test/java/tomorrowme/todo/api/controller/work/BoxControllerTest.java
+++ src/test/java/tomorrowme/todo/api/controller/work/BoxControllerTest.java
@@ -13,6 +13,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import tomorrowme.todo.api.controller.work.dto.request.BoxCreate;
+import tomorrowme.todo.api.controller.work.dto.request.BoxFind;
+import tomorrowme.todo.api.service.work.BoxReadService;
 import tomorrowme.todo.api.service.work.BoxWriteService;
 
 @WebMvcTest(controllers = BoxController.class)
@@ -20,6 +22,8 @@ class BoxControllerTest {
 
     @MockBean
     private BoxWriteService boxWriteService;
+    @MockBean
+    private BoxReadService boxReadService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -60,6 +64,7 @@ class BoxControllerTest {
             .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
     }
+
     @DisplayName("보관함을 생성할 회원의 키워드가 비어있으면 예외가 발생한다.")
     @Test
     void createWithoutKeyword() throws Exception {
@@ -73,6 +78,55 @@ class BoxControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/box")
                 .content(objectMapper.writeValueAsString(boxCreate))
                 .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isBadRequest())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
+    }
+
+    @DisplayName("회원의 전화번호, 키워드로 해당 회원의 모든 보관함을 조회한다.")
+    @Test
+    void findAll() throws Exception {
+        String phone = "01012341234";
+        String keyword = "keyword";
+        BoxFind boxFind = new BoxFind(phone, keyword);
+
+        //when && then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/box")
+                .queryParam("phone", boxFind.phone())
+                .queryParam("keyword", boxFind.keyword()))
+            .andExpect(status().isOk());
+
+    }
+
+
+    @DisplayName("보관함을 조회 할 회원의 전화번호가 비어있으면 예외가 발생한다.")
+    @Test
+    void findAllWithoutPhone() throws Exception {
+        //given
+        String phone = null;
+        String keyword = "keyword";
+        BoxFind boxFind = new BoxFind(phone, keyword);
+
+        //when && then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/box")
+                .queryParam("phone", boxFind.phone())
+                .queryParam("keyword", boxFind.keyword()))
+            .andExpect(status().isBadRequest())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
+    }
+    @DisplayName("보관함을 조회 할 회원의 키워드가 비어있으면 예외가 발생한다.")
+    @Test
+    void findAllWithoutKeyword() throws Exception {
+        //given
+        String phone = "01012341234";
+        String keyword = null;
+        BoxFind boxFind = new BoxFind(phone, keyword);
+
+        //when && then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/box")
+                .queryParam("phone", boxFind.phone())
+                .queryParam("keyword", boxFind.keyword()))
             .andExpect(status().isBadRequest())
             .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
             .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
