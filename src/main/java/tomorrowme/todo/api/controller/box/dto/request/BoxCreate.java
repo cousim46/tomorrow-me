5f8652888d8f9eb0a2a0f10661a076ceb017278e
cousim46 <wjdghldns24@naver.com>
2024-05-21 21:46:38 +0900

    feat : 보관함을 생성하기 위한 Request Dto

---
+++ src/main/java/tomorrowme/todo/api/controller/box/dto/request/BoxCreate.java
@@ -0,0 +1,12 @@
+package tomorrowme.todo.api.controller.box.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record BoxCreate (
+     String title,
+     @NotBlank(message = "전화번호는 필수로 입력해야 합나다.")
+     String phone,
+     @NotBlank(message = "키워드는 필수로 입력해야 합나다.")
+     String keyword
+){
+}
