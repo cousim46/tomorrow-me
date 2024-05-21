0f1181e9a254c23f419b53b37bd66ab546ef8535
cousim46 <wjdghldns24@naver.com>
2024-05-21 21:50:22 +0900

    feat : 보관함 생성 API

---
+++ src/main/java/tomorrowme/todo/api/controller/box/BoxController.java
@@ -0,0 +1,23 @@
+package tomorrowme.todo.api.controller.box;
+
+import jakarta.validation.Valid;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tomorrowme.todo.api.controller.box.dto.request.BoxCreate;
+import tomorrowme.todo.api.service.work.BoxWriteService;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/box")
+@RestController
+public class BoxController {
+    private final BoxWriteService boxWriteService;
+
+    @PostMapping
+    public void create(@Valid @RequestBody BoxCreate boxCreate) {
+        boxWriteService.create(boxCreate.phone(), boxCreate.keyword(), boxCreate.title(), LocalDate.now());
+    }
+}
