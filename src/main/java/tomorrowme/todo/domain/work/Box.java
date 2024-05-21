6f01fba1bedde99035383269fbcac6d1b72b3057
cousim46 <wjdghldns24@naver.com>
2024-05-21 19:57:50 +0900

    fix : 보관함을 생성한 고객과 생성 시간 기준으로 유니크 제약 조건 생성 및 보관함 제목 컬럼 추가

--- src/main/java/tomorrowme/todo/domain/work/Box.java
+++ src/main/java/tomorrowme/todo/domain/work/Box.java
@@ -3,6 +3,9 @@ package tomorrowme.todo.domain.work;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,16 +15,25 @@ import tomorrowme.todo.domain.account.Account;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(
+    uniqueConstraints = @UniqueConstraint(
+        name = "box_created_at", columnNames = {"account_id", "registrationDate"}
+    )
+)
 public class Box extends BaseEntity {
+    private String title;
+    private LocalDate registrationDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Account account;
 
-    private Box(Account account) {
+    private Box(String title, Account account, LocalDate registrationDate) {
+        this.title = title;
         this.account = account;
+        this.registrationDate = registrationDate;
     }
 
-    public static Box create(Account account) {
-        return new Box(account);
+    public static Box create(String title,Account account, LocalDate registrationDate) {
+        return new Box(title,account, registrationDate);
     }
 }
