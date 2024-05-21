ad322b8413b632c9b85fef32bd413b02f231474b
cousim46 <wjdghldns24@naver.com>
2024-05-22 00:06:01 +0900

    feat : 보관함 조회에 대한 Request DTO

---
+++ src/main/java/tomorrowme/todo/api/controller/work/dto/request/BoxFind.java
@@ -0,0 +1,11 @@
+package tomorrowme.todo.api.controller.work.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record BoxFind(
+    @NotBlank(message = "전화번호는 필수로 입력해야 합나다.")
+    String phone,
+    @NotBlank(message = "키워드는 필수로 입력해야 합나다.")
+    String keyword
+) {
+}
