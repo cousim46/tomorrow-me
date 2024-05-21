434a88e765fa7c9cc04d2dc368dc9f0f15b36cd4
cousim46 <wjdghldns24@naver.com>
2024-05-21 20:01:25 +0900

    feat : 전화번호와 키워드로 회원 조회하는 쿼리 추가

--- src/main/java/tomorrowme/todo/domain/account/AccountRepository.java
+++ src/main/java/tomorrowme/todo/domain/account/AccountRepository.java
@@ -1,8 +1,10 @@
 package tomorrowme.todo.domain.account;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByPhoneAndKeyword(String phone, String keyword);
 }
