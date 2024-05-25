7c6092a83be4e86e3d19126f1ec409531e367e98
cousim46 <wjdghldns24@naver.com>
2024-05-25 17:36:30 +0900

    test: 보관함 조회 테스트

--- src/test/java/tomorrowme/todo/api/controller/work/BoxControllerTest.java
+++ src/test/java/tomorrowme/todo/api/controller/work/BoxControllerTest.java
@@ -1,8 +1,13 @@
 package tomorrowme.todo.api.controller.work;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +21,7 @@ import tomorrowme.todo.api.controller.work.dto.request.BoxCreate;
 import tomorrowme.todo.api.controller.work.dto.request.BoxFind;
 import tomorrowme.todo.api.service.work.BoxReadService;
 import tomorrowme.todo.api.service.work.BoxWriteService;
+import tomorrowme.todo.api.service.work.dto.response.BoxInfo;
 
 @WebMvcTest(controllers = BoxController.class)
 class BoxControllerTest {
@@ -61,8 +67,8 @@ class BoxControllerTest {
                 .content(objectMapper.writeValueAsString(boxCreate))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
     }
 
     @DisplayName("보관함을 생성할 회원의 키워드가 비어있으면 예외가 발생한다.")
@@ -79,8 +85,8 @@ class BoxControllerTest {
                 .content(objectMapper.writeValueAsString(boxCreate))
                 .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isBadRequest())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
     }
 
     @DisplayName("회원의 전화번호, 키워드로 해당 회원의 모든 보관함을 조회한다.")
@@ -89,13 +95,21 @@ class BoxControllerTest {
         String phone = "01012341234";
         String keyword = "keyword";
         BoxFind boxFind = new BoxFind(phone, keyword);
-
+        given(boxReadService.findAll(phone,keyword)).willReturn(List.of(
+            new BoxInfo(LocalDateTime.of(2024,1,1,1,1,1),"title1",LocalDateTime.of(2024,1,1,1,1,1)),
+            new BoxInfo(LocalDateTime.of(2024,2,1,1,1,1),"title2",LocalDateTime.of(2024,2,1,1,1,1))
+        ));
         //when && then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/box")
                 .queryParam("phone", boxFind.phone())
                 .queryParam("keyword", boxFind.keyword()))
-            .andExpect(status().isOk());
-
+            .andExpect(jsonPath("$[0]").exists())
+            .andExpect(jsonPath("$[1]").exists())
+            .andExpect(
+                jsonPath("$[0].title").value("title1"))
+            .andExpect(jsonPath("$[0].createdAt").value("2024-01-01 01:01:01"))
+            .andExpect(jsonPath("$[1].title").value("title2"))
+            .andExpect(jsonPath("$[1].createdAt").value("2024-02-01 01:01:01"));
     }
 
 
@@ -112,8 +126,8 @@ class BoxControllerTest {
                 .queryParam("phone", boxFind.phone())
                 .queryParam("keyword", boxFind.keyword()))
             .andExpect(status().isBadRequest())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("전화번호는 필수로 입력해야 합나다."));
     }
     @DisplayName("보관함을 조회 할 회원의 키워드가 비어있으면 예외가 발생한다.")
     @Test
@@ -128,7 +142,7 @@ class BoxControllerTest {
                 .queryParam("phone", boxFind.phone())
                 .queryParam("keyword", boxFind.keyword()))
             .andExpect(status().isBadRequest())
-            .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("400"))
-            .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
+            .andExpect(jsonPath("$.code").value("400"))
+            .andExpect(jsonPath("$.message").value("키워드는 필수로 입력해야 합나다."));
     }
 }
